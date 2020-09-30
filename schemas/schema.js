311a039fdcf98d5f3ca00577231e7fbb6f010a2e
arvindeva <arvindeva@gmail.com>
2020-09-30 20:43:05 +0700

    Start character on schema

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,36 +1,37 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from 'part:@sanity/base/schema-creator';
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
-
-// We import object and document schemas
-import blockContent from './blockContent'
-import crewMember from './crewMember'
-import castMember from './castMember'
-import movie from './movie'
-import person from './person'
-import screening from './screening'
-import plotSummary from './plotSummary'
-import plotSummaries from './plotSummaries'
+import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
-  name: 'default',
+  name: 'touhouList',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     // The following are document types which will appear
     // in the studio.
-    movie,
-    person,
-    screening,
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
-    blockContent,
-    plotSummary,
-    plotSummaries,
-    castMember,
-    crewMember
-  ])
-})
+    {
+      // This is the display name for the type
+      title: 'Character',
+      // The identifier for this document type used in the api's
+      name: 'character',
+      // Documents have the type 'document'. Your schema may describe types beyond documents
+      // but let's get back to that later.
+      type: 'document',
+      // Now we proceed to list the fields of our document
+      fields: [
+        // This document has only one field
+        {
+          // The display name for this field
+          title: 'Name',
+          // The identifier for this field used in the api's
+          name: 'name',
+          // The type of this field
+          type: 'string',
+        },
+      ],
+    },
+  ]),
+});
